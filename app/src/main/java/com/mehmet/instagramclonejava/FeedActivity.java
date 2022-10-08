84e6cc4dd901c8fc02f2cea934e3b23e5923dda2
Mehmet Emin Yagli <m.eminyagli@gmail.com>
2022-10-08 21:42:11 +0300

    kullanıcı arayüzleri eklendi user interfaces added

--- app/src/main/java/com/mehmet/instagramclonejava/FeedActivity.java
+++ app/src/main/java/com/mehmet/instagramclonejava/FeedActivity.java
@@ -3,23 +3,12 @@ package com.mehmet.instagramclonejava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class FeedActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    public  void SigninClicked(View view){
-
-    }
-
-
-    public  void  SignupClicked (View view){
-
+        setContentView(R.layout.activity_feed);
     }
 }
--- app/src/main/java/com/mehmet/instagramclonejava/UploadActivity.java
+++ app/src/main/java/com/mehmet/instagramclonejava/UploadActivity.java
@@ -5,21 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class UploadActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_upload);
     }
-
-
-    public  void SigninClicked(View view){
+    public  void  UploadButton(View view){
 
     }
-
-
-    public  void  SignupClicked (View view){
+    public  void  SelectImage(View view){
 
     }
 }
